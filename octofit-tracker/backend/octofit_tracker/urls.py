2cac3bb4b2bd8f8b9b54fb22c365eef49f0dde95
hagua <hugo.msagua@gmail.com>
2026-02-19 15:47:36 +0000

    Add REST API endpoint documentation to URL configuration

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -2,6 +2,12 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
+
+REST API Endpoints:
+    The API is accessible at:
+    - Codespace: https://$CODESPACE_NAME-8000.app.github.dev/api/
+    - Localhost: http://localhost:8000/api/
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
